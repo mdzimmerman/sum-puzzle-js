7e1fbc6b6d8d3da013c3661b649afd9af2496e49
Matthew Zimmerman <matt@triclinic.org>
2014-03-29 15:40:55 -0400

    Generated an inital version of the objects used for board generation.

--- puzzle.js
+++ puzzle.js
@@ -1,12 +1,54 @@
-var board=document.getElementById("board");
-var boardCtx=board.getContext("2d");
-var cellSize=50;
-
-boardCtx.beginPath();
-for (i=0; i<=9; i++) {
-	boardCtx.moveTo(1,i*cellSize+1);
-	boardCtx.lineTo(451,i*cellSize+1);
-	boardCtx.moveTo(i*cellSize+1,1);
-	boardCtx.lineTo(i*cellSize+1,452);
+function Cell(x, y) {
+    this.x = x;
+    this.y = y;
 }
-boardCtx.stroke();
+
+function Board(canvasId) {
+    this.board = document.getElementById(canvasId);
+    this.nCells = 9;
+    this.cellPixel = 50;
+    this.widthPixel = this.nCells*this.cellPixel+1;
+    this.heightPixel = this.nCells*this.cellPixel+1;
+    this.reset();
+}
+
+Board.prototype.reset = function() {
+    // reset the whole board
+    this.board.width = this.widthPixel;
+    this.board.height = this.heightPixel;
+    this.boardCtx = this.board.getContext("2d");
+
+    // draw the grid lines
+    this.boardCtx.beginPath();
+    for (var i=0; i<=this.nCells; i++) {
+	    this.boardCtx.moveTo(0,i*this.cellPixel+0.5);
+	    this.boardCtx.lineTo(this.widthPixel,i*this.cellPixel+0.5);
+	    this.boardCtx.moveTo(i*this.cellPixel+0.5,0);
+	    this.boardCtx.lineTo(i*this.cellPixel+0.5,this.heightPixel);
+    }
+    this.boardCtx.stroke();
+}
+
+Board.prototype.setTextLargeCell = function(cell, text) {
+    var cellCanvasX = cell.x * this.cellPixel + this.cellPixel/2;
+    var cellCanvasY = cell.y * this.cellPixel + this.cellPixel/2;
+    this.boardCtx.font = "36px serif";
+    this.boardCtx.textAlign = "center";
+    this.boardCtx.textBaseline = "middle";
+    this.boardCtx.fillText(text, cellCanvasX, cellCanvasY);
+}
+
+
+
+var b = new Board("board");
+var n=1;
+var c;
+for (i=0; i<b.nCells; i++) {
+    for (j=0; j<b.nCells; j++) {
+        c = new Cell(i, j);
+        b.setTextLargeCell(c,n);
+        n++;
+        if (n>b.nCells) n=1;
+    }
+}
+
